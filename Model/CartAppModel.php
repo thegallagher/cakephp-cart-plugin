4f887ff0818071d62ca98089fb5a2cd8312196f8
Florian Krämer <florian.kraemer@cakedc.com>
2012-08-21 00:08:41 +0200

    Adding a way to change the plugin model associations via configuration. This is needed in the cases were you want to use another model class than 'User' for example for the 'User' associations in the cart models.

--- Model/CartAppModel.php
+++ Model/CartAppModel.php
@@ -16,4 +16,45 @@ class CartAppModel extends AppModel {
 	public $actsAs = array(
 		'Containable');
 
+/**
+ * Constructor
+ *
+ * @param mixed $id Model ID
+ * @param string $table Table name
+ * @param string $ds Datasource
+ * @access public
+ */
+	public function __construct($id = false, $table = null, $ds = null) {
+		parent::__construct($id, $table, $ds);
+		$this->__configureAssociations();
+	}
+
+/**
+ * This is a workaround to let you define other models in the associations of
+ * the cart plugin without touching it's model code.
+ *
+ * You could for example extend the Order model with AppOrder and then configure
+ * the plugin to use that model by Configure::write('Cart.models.Order', 'AppOrder');
+ *
+ * @return void
+ */
+	protected function __configureAssociations() {
+		$models = Configure::read('Cart.models');
+		if (!empty($models)) {
+			$types = array('belongsTo', 'hasMany', 'hasOne', 'hasAndBelongsToMany');
+			foreach ($models as $model => $modelAssoc) {
+				foreach ($types as $type) {
+					if (isset($this->{$type}[$model])) {
+						if (is_string($modelAssoc)) {
+							$modelAssoc = array('className' => $modelAssoc);
+						}
+						$fields = array_keys($modelAssoc);
+						foreach ($fields as $field) {
+							$this->{$type}[$model][$field] = $modelAssoc[$field];
+						}
+					}
+				}
+			}
+		}
+	}
 }
